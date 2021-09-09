60f4ee60a30b2cae1c7534546ad5036e2c397030
Kasper <Kasper53444@gmail.com>
2021-09-09 10:20:13 +0200

    add stickman

---
+++ stickman/sketch.js
@@ -0,0 +1,34 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  
+  
+
+  
+  circle(180, 70, 23);
+  
+  fill('red')
+  circle(175,68,3)
+  circle(185,68,3)
+  
+  strokeWeight(2)
+  line(180,82,180,140)
+  line(180,110,140,90)
+  line(220,90,180,110)
+  line(210,170,180,140)
+  line(145,170,180,140)
+  
+  
+  
+  stroke('purple')
+  line(175,73,185,74)
+
+  
+  
+noloop
+  
+  
+}
